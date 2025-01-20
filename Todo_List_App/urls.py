82419fd331c76ada5349d31d0d891d7c3f4792df
niraj-commits <karki.ni018@gmail.com>
2025-01-20 21:11:15 +0545

    CRUD operation in Django

--- Todo_List_App/urls.py
+++ Todo_List_App/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',home_page),
-    path('about/',about),
-    path('contact/',contact),
     path('task/',task),
+    path('task/create/',create_tasks),
 ]
--- Todo_List_App/views.py
+++ Todo_List_App/views.py
@@ -1,53 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
 
 # Create your views here.
-def home_page(request):
-    person = [
-        {
-            "name": "Ram",
-            "age":43,
-        },
-        {
-            "name": "Sita",
-            "age":63,
-        },
-        {
-            "name": "GIta",
-            "age":8,
-        },
-        {
-            "name": "Rita",
-            "age":23,
-        },
-        ]
-    context = {"name":"Home Page","persons":person}
-    return render(request,'home_page.html',context)
-
-def about(request):
-    context = {"name":"This app was made by niraj"}
-    return render(request,'about.html',context)
-
-def contact(request):
-    contacts = [
-        {"name":"Ram","Phone":9808978900},
-        {"name":"Shyam","Phone":89898989},
-        {"name": "Alice", "Phone": 12345},
-        {"name": "Bob", "Phone": 67890},
-        {"name": "Charlie", "Phone": 54321},
-        {"name": "Diana", "Phone": 98765}
-    ]
-    context = {"name":"Contact Page","contact":contacts}
-    return render(request,'contact.html',context)
-
-def index(request):
-    context = {"name":"index page"}
-    return render(request,'index.html',context)
-    
 
 def task(request):
     
     task = todo_list.objects.all()
     context = {"tasks":task}
     return render(request,'task.html',context)
+
+def create_tasks(request):
+    
+    if request.method == "POST":
+        name = request.POST.get('name') #returns data in dictionary as payload
+        description = request.POST.get('description')
+        todo_list.objects.create(Name= name,Description = description)
+        return redirect('task/')
+        
+    
+    return render(request,'create_task.html')
+
+def change_status(request,pk):
+    task = todo_list.objects.get(pk == pk)
+    task.status = True
+    task.save()
+    return redirect('task.html')
+    
